6d70309b48abfff4224c0a9d9fb35520da1e870c
Moboluwarin <kolaogunnaike@gmail.com>
2017-07-11 19:17:35 +0200

    created controller for artist and views

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+artist1 = Artist.create!(name:"Michael", remote_image_url:"http://www.uaway.com")
+artist2 = Artist.create!(name:"Miew", remote_image_url:"http://www.uaway.com")
+artist3 = Artist.create!(name:"allo", remote_image_url:"http://www.uaway.com")
+
+song1 = Song.create!(title:"leave", song_url:"http://www.uaway.com", artist:artist1)
+song2 = Song.create!(title:"again", song_url:"http://www.uaway.com", artist:artist2)
